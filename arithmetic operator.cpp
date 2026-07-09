cb79fac733119d873f5836f76cb6b190337d20f1
Jaisuriya <192421167.simats@saveetha.com>
2026-07-09 11:13:56 +0530

    Add files via upload

---
+++ arithmetic operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b;
+	cout<<"Enter the frist number:";
+	cin>>a;
+	cout<<"Enter the second number:";
+	cin>>b;
+	cout<<"ADDITION: "<<a+b<< endl;
+	cout<<"SUBTRACTION: "<<a-b<< endl;
+	cout<<"MULTIPLICATION: "<<a*b<< endl;
+	cout<<"DIVIDE: "<<a/b<< endl;
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"1st num";
+	cin>> a;
+	cout<<"2rd num";
+	cin>> b;
+	cout<<"SUM="<<a+b<< endl;
+	
+}
